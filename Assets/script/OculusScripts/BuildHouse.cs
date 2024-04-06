f9b84de15173eb58821d428771f6b6789752cc00
qulick <58820014+Qulick-k@users.noreply.github.com>
2024-04-07 00:21:03 +0800

    初步完成場景2-4的log紀錄
    [Update] 在BuildHouse腳本新增public UnityEvent<string> saveBuildMissionEvent;，當建築完普羅民遮城之後，就就呼叫SaveSystemCh2的LogMissionComplete()記錄使用者完成哪個任務
    
    [Update] 新增各區域_碰撞體群組

--- Assets/script/OculusScripts/BuildHouse.cs
+++ Assets/script/OculusScripts/BuildHouse.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class BuildHouse : MonoBehaviour
 {
+    //使用者每按一次語音時，當saveBuildMissionEvent有訂閱者的話，就呼叫訂閱者的LogMissionComplete()記錄使用者完成哪個任務
+    public UnityEvent<string> saveBuildMissionEvent;
+
     public GameObject[] houses;
     public int index;
     public GameObject endCanvas, PL, buildCanvas;
@@ -25,10 +29,12 @@ public class BuildHouse : MonoBehaviour
             if (KeepData.guideSwitch)
             {
                 Mission5.text = "<color=green>✓ 5.為荷蘭人造出普羅民遮城(建造普羅民遮城)</color>";
+                saveBuildMissionEvent.Invoke("成功完成，5.為荷蘭人造出普羅民遮城(建造普羅民遮城)");
             }
             else
             {
                 Mission5Com.text = "<color=green>✓ 5.為荷蘭人造出普羅民遮城(建造普羅民遮城)</color>";
+                saveBuildMissionEvent.Invoke("成功完成，5.為荷蘭人造出普羅民遮城(建造普羅民遮城)");
             }
             
         }
